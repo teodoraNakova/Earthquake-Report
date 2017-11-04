9ac4e272d2b69828d3391d00c4845e119e6489f2
Teodora <t_nakova_1991@abv.bg>
2017-11-04 10:38:06 +0200

    add intent to display web page for every earthquake

--- app/src/main/java/com/example/android/earthquakereport/Earthquake.java
+++ app/src/main/java/com/example/android/earthquakereport/Earthquake.java
@@ -7,11 +7,13 @@ public class Earthquake {
     private double mMagnitude;
     private String mLocation;
     private long mTimeInMilliseconds;
+    private String mUrl;
 
-    public Earthquake (double magnitude, String location, long timeInMilliseconds) {
+    public Earthquake (double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
     public double getMagnitude() {
@@ -24,5 +26,9 @@ public class Earthquake {
 
     public long getTimeInMilliseconds() {
         return mTimeInMilliseconds;
+    }
+
+    public String getmUrl() {
+        return mUrl;
     }
 }
--- app/src/main/java/com/example/android/earthquakereport/MainActivity.java
+++ app/src/main/java/com/example/android/earthquakereport/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.earthquakereport;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,17 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake(7.333, "42 km W of San Francisco", 1793459375));
-        earthquakes.add(new Earthquake(7.333, "San Francisco", 1793459375));
-        earthquakes.add(new Earthquake(1.0, "San Francisco", 1793459375));
-        earthquakes.add(new Earthquake(5.2, "San Francisco", 1793459375));
-        earthquakes.add(new Earthquake(3.2, "San Francisco", 1793459375));
-        earthquakes.add(new Earthquake(4.2, "San Francisco", 1793459375));
-        earthquakes.add(new Earthquake(10.2, "San Francisco", 1793459375));
+        final ArrayList<Earthquake> earthquakes = new ArrayList<>();
+        earthquakes.add(new Earthquake(7.333, "42 km W of San Francisco", 1793459375, "https://www.youtube.com/"));
+        earthquakes.add(new Earthquake(7.333, "San Francisco", 1793459375, "https://www.youtube.com/"));
+        earthquakes.add(new Earthquake(1.0, "San Francisco", 1793459375, "https://www.youtube.com/"));
+        earthquakes.add(new Earthquake(5.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
+        earthquakes.add(new Earthquake(3.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
+        earthquakes.add(new Earthquake(4.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
+        earthquakes.add(new Earthquake(10.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
 
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Earthquake quake = earthquakes.get(i);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(quake.getmUrl()));
+                startActivity(intent);
+            }
+        });
     }
 }
