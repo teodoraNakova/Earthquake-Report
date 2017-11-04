f80c0cb3d750a4e381d9896e753620756145480a
Teodora <t_nakova_1991@abv.bg>
2017-11-04 13:21:07 +0200

    add EarthquakeAsyncTask class in main activity class

--- app/src/main/java/com/example/android/earthquakereport/MainActivity.java
+++ app/src/main/java/com/example/android/earthquakereport/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.earthquakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,34 +10,58 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String USGS_REQUEST_URL =
+            " https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&" +
+                    "eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    private EarthquakeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake(7.333, "42 km W of San Francisco", 1793459375, "https://www.youtube.com/"));
-        earthquakes.add(new Earthquake(7.333, "San Francisco", 1793459375, "https://www.youtube.com/"));
-        earthquakes.add(new Earthquake(1.0, "San Francisco", 1793459375, "https://www.youtube.com/"));
-        earthquakes.add(new Earthquake(5.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
-        earthquakes.add(new Earthquake(3.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
-        earthquakes.add(new Earthquake(4.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
-        earthquakes.add(new Earthquake(10.2, "San Francisco", 1793459375, "https://www.youtube.com/"));
-
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
         ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
+        listView.setAdapter(mAdapter);
+
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Earthquake quake = earthquakes.get(i);
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(quake.getmUrl()));
-                startActivity(intent);
+                Earthquake currentEarthquake = mAdapter.getItem(i);
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                startActivity(websiteIntent);
             }
         });
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> data) {
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
+
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+        }
     }
 }
